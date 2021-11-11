3863a77e94c0d7a3d51ff56f8ab87291128a5e4e
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-11-11 14:32:51 +0100

    add the router componets to the app

--- seogram/src/App.js
+++ seogram/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-import Home from './html/Home.js'
+import Home from './html/Home.js';
+import { BrowserRouter as Router,Routes, Route, Switch } from 'react-router-dom';
+import Service from './html/Service.js';
+import Blog from './html/Blog';
+import Contact from './html/Contact.js';
+import About from './html/About.js';
 
 function App() {
   return (
+    
+    <Router>
     <div className="App">
-      <Home/>
-    </div>
+      <Route path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/service" component={Service} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/contact" component={Contact} />
+      
+    </Router>
+    
   );
 }
 
