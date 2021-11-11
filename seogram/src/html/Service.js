56a87f99f212d999a0530f69dbebed20bd60a3e5
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-11-11 14:22:44 +0100

    texting out the route component, wote some html to render

---
+++ seogram/src/html/Service.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Service() {
+    return (
+        <div>
+        This is the <h3>service</h3> page
+            
+        </div>
+    )
+}
+
+export default Service
