b80a9a3c586d4e12ff6724d20a6a16ec444cce43
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-11-11 14:19:31 +0100

    texting out the route component, wote some html to render

---
+++ seogram/src/html/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Blog() {
+    return (
+        <div>
+            This is the <h3>blog</h3> Page
+        </div>
+    )
+}
+
+export default Blog
---
+++ seogram/src/html/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div>
+            This is <h3>Contact</h3> page
+        </div>
+    )
+}
+
+export default Contact
