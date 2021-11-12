8368cedb0bcde2f41926662edb724aeac9eb7b4d
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-11-12 02:54:22 +0100

    the navigation is working fine now

--- seogram/src/App.js
+++ seogram/src/App.js
@@ -1,25 +1,28 @@
 import './App.css';
 import Home from './html/Home.js';
-import { BrowserRouter as Router,Routes, Route, Switch } from 'react-router-dom';
+import { BrowserRouter,Routes, Route} from 'react-router-dom';
 import Service from './html/Service.js';
 import Blog from './html/Blog';
 import Contact from './html/Contact.js';
 import About from './html/About.js';
+import Navigate from './html/Navigate.js';
 
 function App() {
   return (
-    
-    <Router>
     <div className="App">
-      <Route path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/service" component={Service} />
-      <Route path="/blog" component={Blog} />
-      <Route path="/contact" component={Contact} />
-      
-    </Router>
+          <Navigate/>
+          <BrowserRouter>
+              <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/about" element={<About/>} />
+                    <Route path="/service" element={<Service/>} />
+                    <Route path="/blog" element={<Blog/>} />
+                    <Route path="/contact" element={<Contact/>}/>
+              </Routes>
+           </BrowserRouter>
+           </div>
     
-  );
+  )
 }
 
 export default App;
