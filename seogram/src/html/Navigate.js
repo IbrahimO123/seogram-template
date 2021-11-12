e7f79cb821f16812b25d2d4db325bc7243d936fa
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-11-12 03:02:18 +0100

    Without adding the link the nav bar is working well, react router have really make some progress on this

--- seogram/src/html/Navigate.js
+++ seogram/src/html/Navigate.js
@@ -1,12 +1,43 @@
 import React from 'react';
 
-
-function Naviagte() {
+function Navigate() {
 
 
     return (
         <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
+      <div className="container">
+        <a href="####" className="navbar-brand">Seo<span className="per1">Gram.</span></a>
 
+        <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="navbar-collapse collapse" id="navbarContent">
+          <ul className="navbar-nav ms-auto justify-content-around">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/about">About</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/service">Services</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/blog">Blog</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">Contact</a>
+            </li>
+            <li className="nav-item">
+              <a className="btn bg text-white ml-lg-2" href="/blog">Free Analytics</a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
         </>
     )
 }
