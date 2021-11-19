3f739cbb127946dad6c8a885321232f1e8ce68e9
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-11-20 00:06:25 +0100

    add some animation to the page also rearranged the components

--- seogram/src/html/Home.js
+++ seogram/src/html/Home.js
@@ -31,7 +31,7 @@ function Home() {
         <div  className="container" >
             <div className="jumbotron jumbotron-fluid bg-light mt-4 py-5 banner2">
             <div className="row align-items-center flex-wrap-reverse h-100" >
-             <div  className="col-md-6 py-5">
+             <div  className="col-md-6 py-5 wow animate__animated animate__fadeInLeft">
                 <div className="px-5 mb-4" id='banner1' >
                 <h1 className="text-secondary">Let's  Check and Optimise your website! </h1>
                 <p className="text-lg text-muted" >Ignite the most powerful growth engine you have ever built for your company</p>
@@ -48,7 +48,7 @@ function Home() {
                 </div>
                 </div>
                 </div>
-                <div className="col-md-6" >
+                <div className="col-md-6 wow animate__animated animate__zoomIn" >
                   <div>
                   <img className="img img-fluid rounded-sm" style={styleImg1} alt="banner-1" src={banner1}></img> 
                   </div>
@@ -72,7 +72,7 @@ function Home() {
 
         {/****************Our Services Check *******************/}
             <div className="services w-100 text-center py-5 " >
-                <div className="services py-5" style={styleService} >
+                <div className="services py-5 wow animate__animated animate__fadeInUp" style={styleService} >
                  <label className="d-block h3 text-white" >Check your Website SEO:</label>
                  <div className="input-group w-50 mx-auto py-3">
                        <input className="form-control w-50 mx-auto" placeholder="E.g google.com" type="text" ></input>
@@ -87,7 +87,7 @@ function Home() {
             <div className="bg2 py-5 text-white" style={styleService} >
             <div className="container ">
               <div className="row align-items-center" >
-            <div className="col-lg-6 py-3 pr-lg-5 ">
+            <div className="col-lg-6 py-3 pr-lg-5 wow animate__animated animate__fadeInUp">
                        <div>
                               <div className="subhead" ></div>
                                 <h2 className=" w-75 " >SEO to Improve Brand Visibility</h2>
@@ -105,7 +105,7 @@ function Home() {
                              </ul>
                         </div>
                    </div>
-                   <div className="col-lg-6 py-3 pr-lg-5 mt-3 ml-4 text-center" >
+                   <div className="col-lg-6 py-3 pr-lg-5 mt-3 ml-4 text-center wow animate__animated animate__fadeInRight" >
                        <p className=""><img className="img-responsive" src={banner2} alt="" ></img></p>
                    </div>
               </div>
